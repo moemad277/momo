2fce78741edcb320c285663f2b030bc76412ecfe
moemad277 <154763276+moemad277@users.noreply.github.com>
2024-01-11 23:54:47 +0200

    Update 20240108234540_setup.sql
    secrets updated

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,9 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+ project_url TEXT := 'https://zdwzubiainrvdweweyuo.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpkd3p1YmlhaW5ydmR3ZXdleXVvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNTAwNDU5MiwiZXhwIjoyMDIwNTgwNTkyfQ.SwjoiqPp9EKrO0G7bwfYh2IX3oNEQxncGtwqhA7DlNc'; -- full access needed for http request to storage
+  url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
